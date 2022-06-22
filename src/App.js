87c705419e5303cb1bbc3f7d096e6d5b93ff7e8c
lye <lyeclyde@gmail.com>
2022-06-22 14:00:22 +0545

    scroll-animation

--- src/App.js
+++ src/App.js
@@ -4,11 +4,23 @@ import explore from './assets/lon.jpg';
 import explore1 from './assets/sce.jpg';
 import explore2 from './assets/zee.jpg';
 import Main from './components/Main';
+import Slider from './components/Slider';
 
 function App() {
   return (
   <div className="App">
-    <Main  />
+    <Main  imageSrc={explore2}/>
+    <Slider 
+    imageSrc={explore1} 
+    title={"Be an douche"} 
+    subtitle={"This is where we all say goodbye to each other!"}
+    />
+    <Slider 
+    imageSrc={explore} 
+    title={"Feels like 2020"} 
+    subtitle={"This is where we all say goodbye to each other!"}
+    flipped={true}
+    />
   </div>
   );
 }
